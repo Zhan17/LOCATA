b6e1f0e9b1c6479a54442e59add112b916c57042
zhan22 <98624130+zhan22@users.noreply.github.com>
2023-04-24 02:53:41 -0700

    Update Locata_Main.py

--- Locata_Main.py
+++ Locata_Main.py
@@ -264,7 +264,8 @@ class Locata_Main:
         dropdown=['Select']
         for i in range(len(col_list)):
             dropdown.append(col_list[i])
-        business_type = st.selectbox('',dropdown)
+        dropdown=sorted(dropdown)
+        business_type = st.selectbox('Top 50 Business Type',dropdown)
 
         price_business_list = ['Mexican', 'Coffee & Tea', 'Nail Salons', 'Hair Salons', 'Sandwiches', 'Fast Food', 'Bars', 'Thai']
         
